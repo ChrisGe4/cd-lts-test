1c6b67f0663a4e80311d52ac881b37097489ee4d
chrisge4 <b47073c2c2696dbae4e6e3dcb8449e535876ccb0>
2021-11-29 16:40:48 -0500

    add github auth

--- scripts/report.sh
+++ scripts/report.sh
@@ -7,10 +7,14 @@ OS_VULN_FILE=/workspace/os_vuln.txt
 if [ -z "$_OS_VULN_LABEL" ]; then
   _OS_VULN_LABEL="lts os vuln"
 fi
+# TODO: change to skaffold repo
+if [ -z "$_REPO" ]; then
+  _REPO="ChrisGe4/cd-lts-test"
+fi
 check_existing_issue() {
   label=$1
 
-  if [ $(gh issue list --label="$label" --repo="ChrisGe4/cd-lts-test"| wc -c) -ne 0 ]; then
+  if [ $(gh issue list --label="$label" --repo="$_REPO" | wc -c) -ne 0 ]; then
     echo "There is already an issue opened for the detected vulnerabilities in the LTS images." && exit 0
   fi
 }
@@ -19,10 +23,12 @@ create_issue() {
   title="$1"
   body_file="$2"
   label="$3"
-  echo gh issue create --title="$title" --label="$label" --body-file="$body_file" --body="Vulnerabilities have been found in the following images:" --repo="ChrisGe4/cd-lts-test"
-  gh issue create --title="${title}" --label="${label}" --body-file="$body_file" --body="Vulnerabilities have been found in the following images:"
+#  echo gh issue create --title="$title" --label="$label" --body-file="$body_file" --body="Vulnerabilities have been found in the following images:" --repo="ChrisGe4/cd-lts-test"
+  gh issue create --title="${title}" --label="${label}" --body "$(cat "$body_file")" --repo="$_REPO"
+  #  --body="Vulnerabilities have been found in the following images:"
 }
 
+gh auth login --with-token < token.txt
 check_existing_issue "$_OS_VULN_LABEL"
 echo "creating an issue"
 create_issue "$TITLE_OS" "$OS_VULN_FILE" "$_OS_VULN_LABEL"
